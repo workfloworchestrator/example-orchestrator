fd12e38d4a5c6644d3287a9537d2b9bd23531189
Hans Trompert <hans.trompert@surf.nl>
2023-11-23 17:30:37 +0100

    add copyright notice

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-# import uvicorn
+# Copyright 2019-2023 SURF.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from orchestrator import OrchestratorCore
 from orchestrator.cli.main import app as core_cli
 from orchestrator.settings import AppSettings
@@ -10,4 +23,3 @@ app = OrchestratorCore(base_settings=AppSettings())
 
 if __name__ == "__main__":
     core_cli()
-    # uvicorn.run(app, host="127.0.0.1", port=8080)
--- utils/singledispatch.py
+++ utils/singledispatch.py
@@ -11,6 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 from itertools import filterfalse
 from typing import Any, Callable, NoReturn
 
