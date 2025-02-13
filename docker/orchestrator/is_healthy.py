8e608a53c12472c7695fbeff0631cb05d40b6dd3
Mark90 <mark_moes@live.nl>
2025-02-13 13:26:12 -0800

    Add trailing slash to orchestrator health check to stop 307s

--- docker/orchestrator/is_healthy.py
+++ docker/orchestrator/is_healthy.py
@@ -3,4 +3,4 @@
 import requests
 
 # Exit with status code >0 on non-200 response
-requests.get("http://127.0.0.1:8080/api/health").raise_for_status()
+requests.get("http://127.0.0.1:8080/api/health/").raise_for_status()
