6495a0c2e4b72b870561747c5d6408f1a7631e85
Mark90 <mark_moes@live.nl>
2025-09-08 15:09:20 +0200

    Use uv instead of pip, bump core from 4.0.4 to 4.4.0rc3

--- docker/orchestrator/entrypoint.sh
+++ docker/orchestrator/entrypoint.sh
@@ -4,11 +4,13 @@
 PATH=$PATH:~/.local/bin
 
 # Install extra requirements for orchestrator
-pip install -r requirements.txt
+pip install uv
+uv sync
+source .venv/bin/activate
 
 if [ -f ${CORE_OVERRIDE}/pyproject.toml ]; then
     echo "⏭️ Use editable install of orchestrator-core with dev and test dependencies"
-    pip install -e $CORE_OVERRIDE[dev,test]
+    uv pip install -e $CORE_OVERRIDE[dev,test]
 
     # Run any missing migrations on the database
     python main.py db upgrade heads
@@ -18,6 +20,6 @@ else
     # Run any missing migrations on the database
     python main.py db upgrade heads
 
-    echo "⏭️ Use pip installed orchestrator-core $(pip freeze | grep orchestrator-core)"
+    echo "⏭️ Use orchestrator-core as specified in pyproject.toml $(uv pip freeze | grep orchestrator-core)"
     uvicorn --host 0.0.0.0 --port 8080 $UVICORN_ARGS main:app --reload --proxy-headers
 fi
