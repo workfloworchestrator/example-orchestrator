920189ab3237d85323d0a36f7822da8c5eb902ff
Mark90 <mark_moes@live.nl>
2026-04-23 14:20:29 +0200

    Update entrypoint to initialize search indexes on start

--- docker/orchestrator/entrypoint.sh
+++ docker/orchestrator/entrypoint.sh
@@ -8,6 +8,17 @@ pip install uv
 uv sync
 source .venv/bin/activate
 
+setup() {
+    # Run any missing migrations on the database
+    python main.py db upgrade heads
+
+    # Build search indexes (idempotent)
+    python main.py index subscriptions
+    python main.py index products
+    python main.py index processes
+    python main.py index workflows
+}
+
 if [ -f ${CORE_OVERRIDE}/pyproject.toml ]; then
     echo "⏭️ Use editable install of orchestrator-core"
 
@@ -22,8 +33,7 @@ if [ -f ${CORE_OVERRIDE}/pyproject.toml ]; then
     echo "Installing with spec: '$install_spec'"
     uv pip install -e "$install_spec"
 
-    # Run any missing migrations on the database
-    python main.py db upgrade heads
+    setup
 
     uvicorn --host 0.0.0.0 --port 8080 $UVICORN_ARGS wsgi:app --reload --proxy-headers \
         --reload-dir $CORE_OVERRIDE \
@@ -33,8 +43,7 @@ if [ -f ${CORE_OVERRIDE}/pyproject.toml ]; then
         --reload-dir utils \
         --reload-dir workflows
 else
-    # Run any missing migrations on the database
-    python main.py db upgrade heads
+    setup
 
     echo "⏭️ Use orchestrator-core as specified in pyproject.toml $(uv pip freeze | grep orchestrator-core)"
     uvicorn --host 0.0.0.0 --port 8080 $UVICORN_ARGS wsgi:app --reload --proxy-headers
